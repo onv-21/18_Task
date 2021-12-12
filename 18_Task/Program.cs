409c7c32043a16a07446cb65aa72129555278130
Администратор <@DESKTOP-PJPICF5>
2021-12-12 23:44:45 +0800

    Добавьте файлы проекта.

---
+++ 18_Task/Program.cs
@@ -0,0 +1,51 @@
+﻿
+string s = "({][}(})]{)[";
+char[] chars = s.ToCharArray();
+
+foreach (char n in chars)
+{
+    Stack<char> myStack = new Stack<char>();
+    switch (n)
+    {
+        case '(':
+            myStack.Push(')');
+            break;
+        case '{':
+            myStack.Push('}');
+            break;
+        case '[':
+            myStack.Push(']');
+            break;
+        case ')':
+            if (')' == myStack.Peek())
+            {
+                myStack.Pop();
+            }
+
+            break;
+        case '}':
+            if ('}' == myStack.Peek())
+            {
+                myStack.Pop();
+            }
+           
+            break;
+        case ']':
+            if (']' == myStack.Peek())
+            {
+                myStack.Pop();
+            }
+
+            break;
+    }
+   
+    Console.WriteLine(myStack);
+}
+
+
+
+
+
+
+
+
